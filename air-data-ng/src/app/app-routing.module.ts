aa372d9bb357c35474499b95b160a93ea7b82b39
Jeff <Jeff59550@gmail.com>
2022-06-07 12:01:25 +0200

    :wrench: : angular 14

--- air-data-ng/src/app/app-routing.module.ts
+++ air-data-ng/src/app/app-routing.module.ts
@@ -1,33 +1,10 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from "./pages/home/home.component";
-import {LoginComponent} from "./features/login/login.component";
-import {LogoutComponent} from "./features/logout/logout.component";
-import {ProfileComponent} from "./pages/profile/profile.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-
-const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'logout',
-    component: LogoutComponent,
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
--- air-data-ng/src/app/app.module.ts
+++ air-data-ng/src/app/app.module.ts
@@ -1,34 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatMenuModule} from "@angular/material/menu";
-import {MatButtonModule} from "@angular/material/button";
-import { NavigationComponent } from './features/navigation/navigation.component';
-import { FooterComponent } from './features/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './features/login/login.component';
-import { LogoutComponent } from './features/logout/logout.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavigationComponent,
-    FooterComponent,
-    HomeComponent,
-    LoginComponent,
-    LogoutComponent,
-    ProfileComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MatMenuModule,
-    MatButtonModule,
-    MatIconModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
