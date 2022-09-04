0f546c2258c0e35ad18f13854f211ed12d270736
Jeff <Jeff59550@gmail.com>
2022-09-04 17:57:55 +0200

    ✅ : add basic testing

---
+++ air-data-ng/cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
+
+module.exports = {
+  projectId: "6gmyep"
+}
